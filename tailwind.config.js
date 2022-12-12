648f88a264a3a2734a6fbf4da8c798430c92fd5f
angelbeat <koleso.vitiy@yandex.ru>
2022-12-12 23:45:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'blackBg': '3px 3px 0px 0px black'
+      },
+      'animation': {
+        'text':'text 5s ease infinite',
+    },
+    'keyframes': {
+        'text': {
+            '0%, 100%': {
+               'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+            '50%': {
+               'background-size':'200% 200%',
+                'background-position': 'right center'
+            },
+        },
+      }
+  },
+  },
+  plugins: [],
+}
