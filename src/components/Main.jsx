d8dfc14fb3b53531315a05e219e5035bd59e9cb8
ange1beat <106840878+ange1beat@users.noreply.github.com>
2023-01-02 22:29:30 +0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -39,7 +39,7 @@ const Main = () => {
             <Header/>
             <div className='flex flex-col bg-black scroll-smooth'>
                 <div className="overflow-hidden flex sm:bg-center justify-center items-center h-screen bg-no-repeat w-screen bg-cover sm:w-auto">
-                    <div className='sm:text-[50px] text-[150%] sm:mb-[25%] mb-[100%] font-bold text-white drop-shadow-md'>Будущее начинается здесь</div>
+                    <div className='sm:text-[50px] text-[150%] sm:mb-[25%] mb-[100%] font-bold text-white drop-shadow-md' data-aos='fade'>Будущее начинается здесь</div>
                 </div>
                 <div id='advt' className='text-center w-100% p-5'>
                     <div className='m-auto text-[200%] sm:text-[300%] font-bold w-max bg-gradient-to-r bg-clip-text text-transparent from-purple-500 via- to-sky-400 animate-text'>ПРЕИМУЩЕСТВА</div>
